3c332310953490d9f63583f0202d4471f5cb3dfd
dianamocanu05 <diana.paula.mocanu@gmail.com>
2021-05-28 21:49:57 +0300

    working on the web application

---
+++ src/main/java/api/MvcConfig.java
@@ -0,0 +1,20 @@
+package api;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName(
+                "forward:/index.html");
+    }
+}
+
+
